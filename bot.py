f1609d2fe69d53247617e5365e451237cf89eb6c
Yumi00000 <vb34550@gmail.com>
2023-06-12 12:24:00 +0200

    add ffmpeg

--- bot.py
+++ bot.py
@@ -1,28 +1,35 @@
 import os
-
-import lyricsgenius
+from shazamio import Shazam
 import spotipy
 import telebot
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 from telebot import types
+import requests
+from lyricsgenius import Genius
+
 
 load_dotenv()
 lg_token = os.getenv('LG_TOKEN')
 telegram_token = os.getenv('TELEGRAM_TOKEN')
 spotify_client_id = os.getenv('SPOTIFY_CLIENT_ID')
 spotify_client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
-genius = lyricsgenius.Genius(lg_token)
 bot = telebot.TeleBot(telegram_token)
+genius = Genius(lg_token)
 spotify_credentials = SpotifyClientCredentials(client_id=spotify_client_id, client_secret=spotify_client_secret)
 spotify = spotipy.Spotify(client_credentials_manager=spotify_credentials)
-
+shazam = Shazam()
 current_index = 0
 query = ''
 button_next = types.InlineKeyboardButton('Next', callback_data='next')
 button_previous = types.InlineKeyboardButton('Previous', callback_data='previous')
+button_lyrics = types.InlineKeyboardButton('Lyrics', callback_data='lyrics')
 keyboard = types.InlineKeyboardMarkup()
 keyboard.row(button_previous, button_next)
+keyboard.add(button_lyrics)
+keyboard_lyrics = types.InlineKeyboardMarkup()
+back_button = types.InlineKeyboardButton('Back', callback_data='back')
+keyboard_lyrics.row(back_button)
 
 
 def get_track_info(track):
@@ -43,16 +50,17 @@ def search_track(query):
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id,
-                     "Hi! I'm Hitori! Your personal music finder. Give me a song name, and I'll find it for you.")
+                     "Hi! I'm Hitori! Your personal music finder. "
+                     "Give me a song name, and I'll find it for you.")
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_message(message):
     global current_index, query
     query = message.text
     if query:
         current_index = 0
-        if query == "":
+        if query is None:
             bot.send_message(message.chat.id, "Give me a song name")
             return
         tracks = search_track(query)
@@ -63,26 +71,60 @@ def handle_message(message):
         bot.send_message(message.chat.id, track_info, reply_markup=keyboard)
 
 
+@bot.message_handler(func=lambda message: True, content_types=['voice'])
+def handle_message(message):
+    query_v = bot.get_file(message.voice.file_id)
+    path = query_v.file_path
+    qname = os.path.basename(path)
+    getting_file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(telegram_token, query_v.file_path))
+    with open(qname, 'wb') as f:
+        f.write(getting_file.content)
+        directory = os.path.join(os.getcwd(), qname)
+        print(directory)
+    for file in os.listdir("C:\IMPORTANT!!!\Hitori"):
+        if file.endswith('.oga'):
+            input_file = os.path.join(  directory)
+            output_file = os.path.splitext(file)[0] + '.mp3'
+            output_path = directory
+            os.system("ffmpeg -i {} -ar 44100 -ac 2 -b:a 192k {}.mp3".format(input_file, output_path))
+    os.remove(os.path.basename(directory))
+
+
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback_query(call):
     global current_index, query
     tracks = search_track(query)
-    if call.data == 'next':
-        current_index += 1
-        if current_index >= len(tracks):
-            current_index = len(tracks) - 1
-            bot.answer_callback_query(call.id, text='No more tracks available.')
-            return
-    elif call.data == 'previous':
-        current_index -= 1
-        if current_index < 0:
-            current_index = 0
-            bot.answer_callback_query(call.id, text='No previous tracks available.')
-            return
-
-    track_info = get_track_info(tracks[current_index])
-    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=track_info,
-                          reply_markup=keyboard)
+    if call.data == 'next' or call.data == 'previous':
+        if call.data == 'next':
+            current_index += 1
+            if current_index >= len(tracks):
+                current_index = len(tracks) - 1
+                bot.answer_callback_query(call.id, text='No more tracks available.')
+                return
+        elif call.data == 'previous':
+            current_index -= 1
+            if current_index < 0:
+                current_index = 0
+                bot.answer_callback_query(call.id, text='No previous tracks available.')
+                return
+        track_info = get_track_info(tracks[current_index])
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text=track_info, reply_markup=keyboard)
+    if call.data == 'lyrics':
+        info = search_track(query)
+        name = info[current_index]['name']
+        artist = info[current_index]['artists'][0]['name']
+        find_song = genius.search_song(f"{name} {artist}")
+        if find_song is None:
+            bot.answer_callback_query(call.id, 'Lyrics not found')
+        else:
+            lyrics = find_song.lyrics
+            if find_song is not None and len(lyrics) <= 4090:
+                bot.send_message(call.message.chat.id, lyrics, reply_markup=keyboard_lyrics)
+            else:
+                bot.answer_callback_query(call.id, 'Lyrics too long')
+    if call.data == 'back':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
 
 
 bot.polling()
